2e4f5ed3a7f29dd60c5bc49c9ecf2cb95b122b55
hasitha madusanka <hasithamadusanka90_@outlook.com>
2018-09-20 10:36:57 +0530

    flex

--- App.js
+++ App.js
@@ -12,40 +12,16 @@ import {
 export default class App extends React.Component {
   render() {
     return (
-      <View>
-        <View>
-          <Text>Positioning</Text>
-        </View>
+      <View style={styles01.mainContainer}>
         <View style={styles01.navbar}>
+          <View ></View>
           <Text style={styles01.navbarTitle}>LK News</Text>
           <Text style={styles01.navbarButton}>Search</Text>
         </View>
-        <View style={styles02.navbar}>
-          <Text style={styles02.navbarTitle}>LK News</Text>
-          <Text style={styles02.navbarButton}>Search</Text>
-        </View>
-        <View style={styles03.navbar}>
-          <Text style={styles03.navbarTitle}>LK News</Text>
-          <Text style={styles03.navbarButton}>Search</Text>
-        </View>
-        <View style={styles04.navbar}>
-          <Text style={styles04.navbarTitle}>LK News</Text>
-          <Text style={styles04.navbarButton}>Search</Text>
-        </View>
-        <View style={styles05.navbar}>
-          <Text style={styles05.navbarTitle}>LK News</Text>
-          <Text style={styles05.navbarButton}>Search</Text>
-        </View>
-        <View>
-          <Text>Resizing</Text>
-        </View>
-        <View style={styles06.navbar}>
-          <Text style={styles06.navbarTitle}>LK News</Text>
-          <Text style={styles06.navbarButton}>Search</Text>
-        </View>
-        <View style={styles07.navbar}>
-          <Text style={styles07.navbarTitle}>LK News</Text>
-          <Text style={styles07.navbarButton}>Search</Text>
+        <View style={styles01.content}>
+          <Text>
+          is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum
+          </Text>
         </View>
       </View>
 
@@ -55,6 +31,13 @@ export default class App extends React.Component {
 
 //Positioning -----------------------------------
 var styles01 = StyleSheet.create({
+  mainContainer: {
+    flex:1
+  },
+  content: {
+    backgroundColor: 'gray',
+    flex: 1
+  },
   navbar: {
      backgroundColor: 'red',
      paddingTop: 30,
@@ -67,117 +50,5 @@ var styles01 = StyleSheet.create({
   },
   navbarButton: {
     color: '#FEFEFE',
-  }
-});
-
-var styles02 = StyleSheet.create({
-  navbar: {
-     backgroundColor: 'blue',
-     paddingTop: 30,
-     paddingBottom: 10,
-     flexDirection: 'column',
-  },
-  navbarTitle: {
-    color: '#FEFEFE',
-    fontWeight: 'bold'
-  },
-  navbarButton: {
-    color: '#FEFEFE',
-  }
-});
-
-var styles03 = StyleSheet.create({
-  navbar: {
-     backgroundColor: 'gray',
-     paddingTop: 30,
-     paddingBottom: 10,
-     flexDirection: 'row',
-     justifyContent: 'center'
-  },
-  navbarTitle: {
-    color: '#FEFEFE',
-    fontWeight: 'bold'
-  },
-  navbarButton: {
-    color: '#FEFEFE',
-  }
-});
-
-var styles04 = StyleSheet.create({
-  navbar: {
-     backgroundColor: 'green',
-     paddingTop: 30,
-     paddingBottom: 10,
-     flexDirection: 'column',
-     justifyContent: 'center'
-  },
-  navbarTitle: {
-    color: '#FEFEFE',
-    fontWeight: 'bold'
-  },
-  navbarButton: {
-    color: '#FEFEFE',
-  }
-});
-
-var styles05 = StyleSheet.create({
-  navbar: {
-     backgroundColor: 'black',
-     paddingTop: 30,
-     paddingBottom: 10,
-     flexDirection: 'column',
-     alignItems: 'center'
-  },
-  navbarTitle: {
-    color: '#FEFEFE',
-    fontWeight: 'bold'
-  },
-  navbarButton: {
-    color: '#FEFEFE',
-  }
-});
-
-
-//Resizing -----------------------------------------
-var styles06 = StyleSheet.create({
-  navbar: {     
-     paddingTop: 30,
-     paddingBottom: 10,
-     flexDirection: 'row',
-     backgroundColor: 'brown', 
-  },
-  navbarTitle: {
-    backgroundColor: 'pink',
-    color: '#FEFEFE',
-    fontWeight: 'bold',
-    textAlign: 'center',
-    flex: 1
-  },
-  navbarButton: {
-    backgroundColor: 'gold',
-    color: '#FEFEFE',
-    textAlign: 'center',
-  }
-});
-
-var styles07 = StyleSheet.create({
-  navbar: {    
-     paddingTop: 30,
-     paddingBottom: 10,
-     flexDirection: 'row',
-     backgroundColor: 'gray', 
-  },
-  navbarTitle: {
-    backgroundColor: 'red',
-    color: '#FEFEFE',
-    fontWeight: 'bold',
-    textAlign: 'center',
-    flex: 4
-  },
-  navbarButton: {
-    backgroundColor: 'green',
-    color: '#FEFEFE',
-    textAlign: 'center',
-    flex: 1
   }
 });
