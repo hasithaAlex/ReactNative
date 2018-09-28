0c9d426eccf997c26c54ad67be3c9450d23289e1
hasitha madusanka <hasithamadusanka90_@outlook.com>
2018-09-28 19:06:31 +0530

    app changes

--- App.js
+++ App.js
@@ -42,26 +42,24 @@ export default class App extends React.Component {
       </View>
 
       <View style={styles.PinMeta}>
-        <View style={styles.MetaText_Container}>
+        <View style={styles.PinMetaText_Container}>
           <Text style={styles.PinMetaText}>Saved from</Text>    
-          <Text style={styles.PinMetaText}>website.com</Text> 
+          <Text style={[styles.PinMetaText,styles.TextBold]}>website.com</Text> 
         </View>
         <View style={styles.VisitButton_Container}>
-          <View style={styles.VisitButton}>
-            <Text style={styles.VisitButtonText}>Visit</Text>
+          <View style={[styles.PinButton, styles.UtilityButton]}>
+            <Text style={[styles.PinButtonText,styles.UtilityButtonText]}>Visit</Text>
           </View>
         </View> 
       </View>
   
       <View style={styles.PinUser}>
         <View style={styles.PinUserAvatar}>
-          <Text>USER_IMAGE</Text>
+          <Text></Text>
         </View>
         <View style={styles.PinUser_Container}>
           <Text style={styles.PinUserText}>
-            <Text style={styles.TextBold}>Alex Kewi</Text>
-            Saved to
-            <Text style={styles.TextBold}>Space</Text>
+            <Text style={styles.TextBold}>Alex Kewi</Text> Saved to <Text style={styles.TextBold}>Space</Text>
           </Text>
           <Text>
             Is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s
@@ -79,7 +77,6 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   Pin_Container: {
     flex: 1,
-    backgroundColor: 'gray',
     alignSelf: 'stretch'
   },
   PinHeader: {
@@ -119,23 +116,53 @@ const styles = StyleSheet.create({
     color: 'white'
   },
   PinContent: {
-    flex: 3,    
-    backgroundColor: 'gray',
+    flex: 4,    
     justifyContent: 'center',
-    alignItems: 'center'
+    alignItems: 'center',
+    paddingLeft: 8,
+    paddingRight: 8
   },
   PinMeta: {
-    backgroundColor: 'white',
+    flex: 1,
     flexDirection: 'row',
     justifyContent: 'space-between',
+    paddingTop: 16,
+    paddingRight: 8,
+    paddingBottom: 16,
+    paddingLeft:8   
   },
   VisitButton_Container:{
     color: 'black'
   },
   ImagePlaceholder: {
+    backgroundColor: '#1e1e1e',
+    flex: 1,
+    alignSelf: 'stretch',
+    borderRadius:6
   },
   PinUser:{
-    flex: 2
+    flex: 5,
+    flexDirection: 'row',
+    paddingRight: 8,
+    paddingLeft:8   
+  }, 
+  UtilityButton:{
+    backgroundColor: '#cecece',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  UtilityButtonText: {
+    color: 'black'    
+  },
+  TextBold: {
+    fontWeight: 'bold'
+  },
+  PinUserAvatar: {
+    width: 50,
+    height: 50,
+    backgroundColor: 'black',
+    borderRadius: 25,
+    marginRight: 8 
   }
 });
  
